58b92ec5f1d545a977c7538ed7e699d816d18328
Pappet <pappet@web.de>
2017-08-22 00:12:50 +0200

    ball Physics

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+	bool isClicking = false;
+	float thrust = 20f;
+
+	Vector3 dir;
+	Vector3 startPos;
+	Vector3 stopPos;
+	Camera cam;
+	Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		cam = Camera.main;
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float kH = Input.GetAxis("Horizontal");
+		float kV = Input.GetAxis("Vertical");
+
+		Vector3 move = new Vector3(kH,kV,0);
+
+		dir = (transform.position - move).normalized;
+
+		rb.AddForce(dir * thrust);
 	}
+
 }
